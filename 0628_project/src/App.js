a48feed59fa8f794c4728f02cf3e1ccc20313754
omer_in <107422716+ome-r@users.noreply.github.com>
2023-07-12 12:08:06 +0900

    feat: drawing

---
+++ 0628_project/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Counter from "./components/Counter";
+import Usecount from "./components/Usecount";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <Usecount />
+    </div>
+  );
+}
+
+export default App;
