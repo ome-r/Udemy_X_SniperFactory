d04380601304a71924e9b1f36415427ebb474ccb
omer_in <107422716+ome-r@users.noreply.github.com>
2023-07-25 19:51:11 +0900

    inhae, feat :: ts tsc

---
+++ learn-typescript/why-ts/sample.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  return a + b;
+}
+
+var result = sum(10, 20);
+result.to; // 자동완성 안됨
---
+++ learn-typescript/why-ts/sample.ts
@@ -0,0 +1,6 @@
+function sum1(a: number, b: number) {
+  return a + b;
+}
+
+var result1 = sum1(10, 20);
+result1.toLocaleString();
